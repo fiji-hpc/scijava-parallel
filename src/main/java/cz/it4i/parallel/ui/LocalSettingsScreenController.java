ff7ebcb817728fc172432380c8bce6466b902f01
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2019-08-05 13:59:17 +0200

    fix local setings screen
    It now returns the correct settings.

--- src/main/java/cz/it4i/parallel/ui/LocalSettingsScreenController.java
+++ src/main/java/cz/it4i/parallel/ui/LocalSettingsScreenController.java
@@ -2,75 +2,42 @@
 package cz.it4i.parallel.ui;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Map;
 
 import cz.it4i.parallel.runners.ImageJServerRunnerSettings;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class LocalSettingsScreenController {
 
 	@FXML
-	private TextField localFijiExecutablePathTextField;
+	public TextField localFijiExecutablePathTextField;
 
 	@FXML
 	public Button okButton;
 
-	private File localFijiExecutablePath;
-
-	private ImageJServerRunnerSettings settings;
-
-	public void fillInputs(final ImageJServerRunnerSettings settings,
-		final Map<String, Object> inputs)
-	{
-		final Path fiji = Paths.get(settings.getFijiExecutable());
-		inputs.put("localDirectory", fiji.getParent().toString());
-		inputs.put("command", fiji.getFileName().toString());
-	}
+	private Stage parentStage;
+	
+	public File localFijiExecutablePath;
+	
+	public ImageJServerRunnerSettings settings;
 
 	private ImageJServerRunnerSettings createSettings() {
-		Path fiji = localFijiExecutablePath.toPath();
-		settings = ImageJServerRunnerSettings.builder().fiji(fiji.toString())
-			.build();
-		return settings;
-	}
-
-	public void run(final Map<String, Object> inputs) {
-//		// Reload settings from input if they already exist:
-//		if (!inputs.isEmpty()) {
-//			this.localDirectory = new File((String) inputs.get("localDirectory"));
-//			this.command = (String) inputs.get("command");
-//		}
-	}
-
-	public ImageJServerRunnerSettings showDialog(
-		final Map<String, Object> inputs)
-	{
-		this.run(inputs);
-		this.openWindow();
-		return settings;
+		Path fijiPath = localFijiExecutablePath.toPath();
+		return ImageJServerRunnerSettings.builder().fiji(fijiPath.toString()).build();
 	}
 
 	@FXML
 	private void browseAction() {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Fiji Executable File");
-		Stage stage = new Stage();
-		File selectedFile = fileChooser.showOpenDialog(stage);
-		if (selectedFile != null) {
-			this.localFijiExecutablePath = selectedFile;
+		File selectedFile = fileChooser.showOpenDialog(this.parentStage);
+		if (selectedFile != null) {			
 			this.localFijiExecutablePathTextField.setText(selectedFile
 				.getAbsolutePath());
 		}
@@ -78,53 +45,31 @@ public class LocalSettingsScreenController {
 
 	@FXML
 	private void okAction() {
-		// Save the settings:
-		this.settings = createSettings();
-
-		// Close the modal window:
-		Stage stage = (Stage) okButton.getScene().getWindow();
-		stage.close();
-	}
-
-	@FXML
-	public void initialize() {
-		localFijiExecutablePathTextField.setText("");
-	}
-
-	private void openWindow() {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-			"local-settings-screen.fxml"));
-		try {
-			Parent fxmlFile = fxmlLoader.load();
-			fxmlLoader.getController();
-			Stage stage = new Stage();
-			stage.initModality(Modality.APPLICATION_MODAL);
-			stage.setResizable(false);
-			stage.setTitle("Local ImageJ Server Settings");
-			stage.setScene(new Scene(fxmlFile));
-			stage.showAndWait();
+		// Check if the selected Fiji executable exists:
+		this.localFijiExecutablePath = new File(this.localFijiExecutablePathTextField.getText());
+		if(!this.localFijiExecutablePath.exists() || this.localFijiExecutablePath.isDirectory()) {
+			showErrorDialog("File selected does not exits!", "Select the fiji executable file!");
+			return;
 		}
-		catch (IOException exc) {
-			showErrorDialog(exc.toString());
+		
+		try{			
+			// Save the settings:
+			this.settings = createSettings();
+			
+			// Close the modal window:
+			Stage stage = (Stage) okButton.getScene().getWindow();
+			stage.close();
+		} catch (Exception exc) {
+			showErrorDialog("Exception", exc.toString());
 		}
+		
 	}
-	
-	private void showErrorDialog(String message) {
+
+	private void showErrorDialog(String header, String message) {
 		Alert alert = new Alert(AlertType.ERROR);
 		alert.setTitle("Error Dialog");
-		alert.setHeaderText(message);
-		alert.setContentText("Ooops, there was an error!");
+		alert.setHeaderText(header);
+		alert.setContentText(message);
 		alert.showAndWait();
 	}
-
-	private void setInitialTextFieldText() {
-		if (this.localFijiExecutablePath == null) {
-			this.localFijiExecutablePathTextField.setText("");
-		}
-		else {
-			this.localFijiExecutablePathTextField.setText(this.localFijiExecutablePath
-				.getAbsolutePath());
-		}
-	}
-
 }
