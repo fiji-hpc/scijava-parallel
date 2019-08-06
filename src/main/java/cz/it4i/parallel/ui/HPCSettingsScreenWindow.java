7ace0870cbee6a1103c884b4ab4098ded5ed1ff1
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2019-08-06 14:06:46 +0200

    clean code event handler using lambda
    Event handler now uses lambda.

--- src/main/java/cz/it4i/parallel/ui/HPCSettingsScreenWindow.java
+++ src/main/java/cz/it4i/parallel/ui/HPCSettingsScreenWindow.java
@@ -1,3 +1,4 @@
+
 package cz.it4i.parallel.ui;
 
 import java.io.IOException;
@@ -12,19 +13,19 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class HPCSettingsScreenWindow {
-	
+
 	private HPCSettingsScreenController controller;
-	
+
 	private HPCSettings settings;
-	
+
 	private void openWindow() {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+		final FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
 			"hpc-settings-screen.fxml"));
 		try {
-			Parent fxmlFile = fxmlLoader.load();
+			final Parent fxmlFile = fxmlLoader.load();
 			this.controller = fxmlLoader.getController();
-			Scene fileSelectionScene = new Scene(fxmlFile);
-			Stage parentStage = new Stage();
+			final Scene fileSelectionScene = new Scene(fxmlFile);
+			final Stage parentStage = new Stage();
 			parentStage.initModality(Modality.APPLICATION_MODAL);
 			parentStage.setResizable(false);
 			parentStage.setTitle("HPC Settings");
@@ -35,14 +36,12 @@ public class HPCSettingsScreenWindow {
 
 			parentStage.showAndWait();
 		}
-		catch (IOException exc) {
+		catch (final IOException exc) {
 			showErrorDialog(exc.toString(), "FXML file is missing!");
 		}
 	}
-	
-	public HPCSettings showDialog(
-		final HPCSettings oldSettings)
-	{
+
+	public HPCSettings showDialog(final HPCSettings oldSettings) {
 		// Get the old settings:
 		if (oldSettings != null) {
 			this.settings = oldSettings;
@@ -50,47 +49,53 @@ public class HPCSettingsScreenWindow {
 
 		// Request new settings:
 		this.openWindow();
-		
+
 		// If the user did not provide new settings:
-		if(controller.getSettings() == null)
-		{
+		if (controller.getSettings() == null) {
 			// Return old settings.
 			return this.settings;
 		}
 		// Return the new settings.
-		return controller.getSettings();		
+		return controller.getSettings();
 	}
-	
+
 	private void setInitialTextFieldText() {
 		if (settings != null) {
 			controller.hostTextField.setText(settings.getHost());
 			controller.portSpinner.getValueFactory().setValue(settings.getPort());
 			controller.userNameTextField.setText(settings.getUserName());
-			//Get authentication choice:
-			if(settings.getAuthenticationChoice().equals("Key file")) {
+			// Get authentication choice:
+			if (settings.getAuthenticationChoice().equals("Key file")) {
 				controller.authenticationChoiceKeyRadioButton.setSelected(true);
 				controller.authenticationChoicePasswordRadioButton.setSelected(false);
 				controller.disableIrrelevantFileds(true);
-			} else {
+			}
+			else {
 				controller.authenticationChoiceKeyRadioButton.setSelected(false);
 				controller.authenticationChoicePasswordRadioButton.setSelected(true);
 				controller.disableIrrelevantFileds(false);
-			}			
-			controller.keyFileTextField.setText(settings.getKeyFile().getAbsolutePath());
-			controller.keyFilePasswordPasswordField.setText(settings.getKeyFilePassword());
+			}
+			controller.keyFileTextField.setText(settings.getKeyFile()
+				.getAbsolutePath());
+			controller.keyFilePasswordPasswordField.setText(settings
+				.getKeyFilePassword());
 			controller.passwordPasswordField.setText(settings.getPassword());
-			controller.schedulerTypeComboBox.getSelectionModel().select(settings.getAdapterType().toString());
-			controller.remoteDirectoryTextField.setText(settings.getRemoteDirectory());
+			controller.schedulerTypeComboBox.getSelectionModel().select(settings
+				.getAdapterType().toString());
+			controller.remoteDirectoryTextField.setText(settings
+				.getRemoteDirectory());
 			controller.commandTextField.setText(settings.getCommand());
 			controller.nodesSpinner.getValueFactory().setValue(settings.getNodes());
 			controller.ncpusSpinner.getValueFactory().setValue(settings.getNcpus());
-			controller.shutdownJobAfterCloseCheckBox.setSelected(settings.isShutdownOnClose());
-			controller.redirectStdOutErrCheckBox.setSelected(settings.isRedirectStdInErr());
+			controller.shutdownJobAfterCloseCheckBox.setSelected(settings
+				.isShutdownOnClose());
+			controller.redirectStdOutErrCheckBox.setSelected(settings
+				.isRedirectStdInErr());
 		}
 	}
-	
-	private void showErrorDialog(String header, String message) {
-		Alert alert = new Alert(AlertType.ERROR);
+
+	private void showErrorDialog(final String header, final String message) {
+		final Alert alert = new Alert(AlertType.ERROR);
 		alert.setTitle("Error Dialog");
 		alert.setHeaderText(header);
 		alert.setContentText(message);
