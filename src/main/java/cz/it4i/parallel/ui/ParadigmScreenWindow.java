0bc52e908c1ee1cedfd2b25bd0a83c8f39857531
Jan Kožusznik <jan@kozusznik.cz>
2019-08-21 20:49:54 +0200

    code: let command be synchronouse

--- src/main/java/cz/it4i/parallel/ui/ParadigmScreenWindow.java
+++ src/main/java/cz/it4i/parallel/ui/ParadigmScreenWindow.java
@@ -1,6 +1,9 @@
 
 package cz.it4i.parallel.ui;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.RunnableFuture;
+
 import org.scijava.parallel.ParadigmManagerService;
 import org.scijava.parallel.ParallelService;
 
@@ -9,7 +12,9 @@ import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ParadigmScreenWindow {
 
 	private ParadigmScreenController controller;
@@ -23,15 +28,25 @@ public class ParadigmScreenWindow {
 	}
 
 	public void openWindow() {
-		JavaFXRoutines.runOnFxThread(() -> {
-			final Scene scene = new Scene(controller);
-			final Stage parentStage = new Stage();
-			parentStage.initModality(Modality.APPLICATION_MODAL);
-			parentStage.setResizable(false);
-			parentStage.setTitle("Pradigm Profiles Manager");
-			parentStage.setScene(scene);
-			parentStage.show();
-		});
+		try {
+			RunnableFuture<Void> f = JavaFXRoutines.runOnFxThread(() -> {
+				final Scene scene = new Scene(controller);
+				final Stage parentStage = new Stage();
+				parentStage.initModality(Modality.APPLICATION_MODAL);
+				parentStage.setResizable(false);
+				parentStage.setTitle("Pradigm Profiles Manager");
+				parentStage.setScene(scene);
+				parentStage.showAndWait();
+			});
+			f.get();
+		}
+		catch (ExecutionException exc) {
+			log.error(exc.getMessage(), exc);
+		}
+		catch (InterruptedException exc) {
+			Thread.currentThread().interrupt();
+			log.error(exc.getMessage(), exc);
+		}
 	}
 
 
