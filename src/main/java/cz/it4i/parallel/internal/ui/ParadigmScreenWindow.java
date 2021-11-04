d288e7418702e8f95d06e03cf4702f6a02d0c9b7
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2021-11-04 13:08:32 +0100

    update swing-javafx-ui dependency and add icon

--- src/main/java/cz/it4i/parallel/internal/ui/ParadigmScreenWindow.java
+++ src/main/java/cz/it4i/parallel/internal/ui/ParadigmScreenWindow.java
@@ -4,12 +4,15 @@ package cz.it4i.parallel.internal.ui;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.RunnableFuture;
 
+import org.kordamp.ikonli.materialdesign.MaterialDesign;
 import org.scijava.parallel.ParallelService;
 
 import cz.it4i.parallel.internal.ParadigmManagerService;
+import cz.it4i.swing_javafx_ui.IconHelperMethods;
 import cz.it4i.swing_javafx_ui.JavaFXRoutines;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
@@ -36,6 +39,9 @@ public class ParadigmScreenWindow {
 				parentStage.setResizable(false);
 				parentStage.setTitle("Paradigm Profiles Manager");
 				parentStage.setScene(scene);
+				Image myImage = IconHelperMethods.convertIkonToImage(
+					MaterialDesign.MDI_ACCOUNT_SETTINGS_VARIANT);
+				parentStage.getIcons().add(myImage);
 				parentStage.show();
 			});
 			f.get();
